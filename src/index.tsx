7889d6a6c0afb6cb18c678a8c31890b0b8a51515
Vyacheslav Zamaraev <zamaraev@gmail.com>
2023-01-27 15:07:40 +0500

    add linter

--- src/index.tsx
+++ src/index.tsx
@@ -17,3 +17,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// "start": "npm run build && node build/main",
+// "build": "rimraf ./build && npx tsc",
